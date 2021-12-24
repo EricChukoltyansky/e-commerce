d9fa1bd598a1d100f5ff5b57cddab6e496163541
EricChukoltyansky <kazohelmet@gmail.com>
2021-12-24 13:58:36 +0200

    first commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -24,3 +24,10 @@ export default class Products extends Component {
     );
   }
 }
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/EricChukoltyansky/e-commerce.git
+// git push -u origin main
